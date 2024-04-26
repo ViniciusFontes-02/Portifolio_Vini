9a23bc3f8d8c41fac750aa5628a75c6a70b23989
Vinicius Fontes <vinif.santos2002@gmail.com>
2024-04-26 11:34:37 -0300

    Criando Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
+import fotoSelfie from "../img/foto-selfie.jpeg";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
 export default function Home() {
-  return <div className="ccccc">Home</div>;
+  return (
+    <div className="home-container">
+      <img src={fotoSelfie} alt="foto selfie" />
+
+      <div className="title">
+        <h1>
+          <span>_</span>Eu sou o Vinícius Fontes
+        </h1>
+
+        <h2>Desenvolvedor Web</h2>
+
+        <p className="text-home">
+          Desde a infância, sou apaixonado por tecnologia, hoje estou
+          constantemente buscando novas oportunidades de aprendizado e
+          crescimento profissional. Meu compromisso primordial é aprimorar
+          minhas habilidades e evoluir continuamente, sempre buscando excelência
+          em meu campo de atuação.
+        </p>
+
+        <Link to="/about" className="see-more-btn">
+          <p>Mais sobre mim!</p>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </Link>
+      </div>
+    </div>
+  );
 }
