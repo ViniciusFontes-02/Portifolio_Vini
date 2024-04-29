8f64133731faa8c8928e0bd2048ce0433845e966
Vinicius Fontes <vinif.santos2002@gmail.com>
2024-04-29 12:06:01 -0300

    Criando esqueleto da pagina sobre mim

---
+++ data/db.jsx
@@ -0,0 +1,58 @@
+export const trainingData = [
+  {
+    name: "Analise e desenvolvimento de sistemas -Estacio",
+    description:
+      "Especializado em Análise e Desenvolvimento de Sistemas, com foco em arquitetura de software e desenvolvimento de aplicações. Experiência em modelagem de dados, gestão de projetos e requisitos.",
+    time: "2023 - 2025",
+    id: 1,
+  },
+  {
+    name: "Curso de TypeScript - Udemy",
+    description:
+      "Curso onde aprendi bastante sobre os conceitos fundamentais e também os mais avançados sobre a utilização da biblioteca React.",
+    time: "04/2024 - 04/2024",
+    id: 2,
+  },
+  {
+    name: "Formação Front-end - HTML, CSS, JavaScript, React e +",
+    description:
+      "Curso onde aprendi bastante sobre os conceitos fundamentais e também os mais avançados sobre a utilização da biblioteca React.",
+    time: "01/2024 - 04/2024",
+    id: 3,
+  },
+  {
+    name: "React do Zero a Maestria (c/ hooks, router, API, Projetos)",
+    description:
+      "Curso onde aprendi bastante sobre os conceitos fundamentais e também os mais avançados sobre a utilização da biblioteca React.",
+    time: "03/2024 - 03/2024",
+    id: 4,
+  },
+  {
+    name: "Certificação Inteligência Artificial - Aplicações Práticas da Hashtag Treinamentos",
+    description:
+      "Curso onde aprendi bastante sobre os conceitos fundamentais e também os mais avançados sobre a utilização da biblioteca React.",
+    time: "03/2024 - 03/2024",
+    id: 5,
+  },
+  {
+    name: "Certificação Javascript Impressionador da Hashtag Treinamentos",
+    description:
+      "Curso onde aprendi bastante sobre os conceitos fundamentais e também os mais avançados sobre a utilização da biblioteca React.",
+    time: "11/2023 - 01/2024",
+    id: 6,
+  },
+  {
+    name: "Certificação Html & Css Impressionador da Hashtag Treinamentos",
+    description:
+      "Curso onde aprendi bastante sobre os conceitos fundamentais e também os mais avançados sobre a utilização da biblioteca React.",
+    time: "08/2023 - 10/2023",
+    id: 7,
+  },
+  {
+    name: "Certificação Python Impressionador da Hashtag Treinamentos",
+    description:
+      "Curso onde aprendi bastante sobre os conceitos fundamentais e também os mais avançados sobre a utilização da biblioteca React.",
+    time: "05/2023 - 08/2023",
+    id: 8,
+  },
+];
