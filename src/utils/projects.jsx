17c0389bfad9a2e91ba4971f1915785906e9e15c
Vinicius Fontes <vinif.santos2002@gmail.com>
2024-05-03 15:59:19 -0300

    adicionando projetos e estilizando scroll

--- src/utils/projects.jsx
+++ src/utils/projects.jsx
@@ -1,4 +1,11 @@
 import imgPizzaria from "../img/PizzaVini.png";
+import imgEcommerce from "../img/ViniStore.png";
+import GeradorDeSenhas from "../img/GeradorDeSenhas.png";
+import ContadorRegressivoReact from "../img/ContadorRegressivoReact.png";
+import ListaTarefas from "../img/ListaTarefasJSeTS.png";
+import GeradordeBoxShadow from "../img/GeradorDeBoxShadow.png";
+import QuizInterativo from "../img/QuizInterativo.png";
+import GeradordeQRCode from "../img/GeradordeQRCode.png";
 
 export const projects = [
   {
@@ -13,79 +20,79 @@ export const projects = [
     id: 1,
   },
   {
-    img: imgPizzaria,
-    title: "PizzaVini",
-    description:
-      "Um site de uma pizzaria onde o cliente pode fazer seu pedido, consultar valores no carrinho, conhecer mais sobre a pizzaria, como historia, localização e contato, e finalizar seu pedido.",
-    repository: "https://github.com/ViniciusFontes-02/Pizzaria_Vini_deploy",
-    deploy: "https://pizza-vini.vercel.app/",
+    img: imgEcommerce,
+    title: "ViniStore",
+    description: "Um site de um ecommerce de produtos eletronicos.",
+    repository:
+      "https://github.com/ViniciusFontes-02/Ecommerce_Responsivo_React.Js",
+    deploy: "https://ecommerce-responsivo-react-js.vercel.app/",
     skill: ["React.Js", "Vite.Js", "CSS3"],
-    createdAt: "17/04/2024",
+    createdAt: "08/04/2024",
     id: 2,
   },
   {
-    img: imgPizzaria,
-    title: "PizzaVini",
+    img: GeradorDeSenhas,
+    title: "Gerador de Senhas",
     description:
-      "Um site de uma pizzaria onde o cliente pode fazer seu pedido, consultar valores no carrinho, conhecer mais sobre a pizzaria, como historia, localização e contato, e finalizar seu pedido.",
-    repository: "https://github.com/ViniciusFontes-02/Pizzaria_Vini_deploy",
-    deploy: "https://pizza-vini.vercel.app/",
-    skill: ["React.Js", "Vite.Js", "CSS3"],
+      "Permite ao usuário personalizar a quantidade de caracteres desejada e incluir letras, números e caracteres especiais. Além disso, oferece a funcionalidade de copiar a senha gerada com apenas um clique.",
+    repository: "https://github.com/ViniciusFontes-02/Gerador-de-senhas-",
+    deploy: "https://viniciusfontes-02.github.io/Gerador-de-senhas-/",
+    skill: ["JavaScript", "HTML5", "CSS3"],
     createdAt: "17/04/2024",
     id: 3,
   },
   {
-    img: imgPizzaria,
-    title: "PizzaVini",
+    img: ContadorRegressivoReact,
+    title: "Contador Regressivo React",
     description:
-      "Um site de uma pizzaria onde o cliente pode fazer seu pedido, consultar valores no carrinho, conhecer mais sobre a pizzaria, como historia, localização e contato, e finalizar seu pedido.",
-    repository: "https://github.com/ViniciusFontes-02/Pizzaria_Vini_deploy",
-    deploy: "https://pizza-vini.vercel.app/",
+      "Os usuários têm a liberdade de personalizar o título, a data, o fundo da tela e a cor do tema. Após submeter as preferências, o sistema redireciona para uma nova página, exibindo o tempo restante até a data escolhida.",
+    repository: "https://github.com/ViniciusFontes-02/ContadorRegressivoReact",
+    deploy: "https://contador-regressivo-react-nine.vercel.app/",
     skill: ["React.Js", "Vite.Js", "CSS3"],
     createdAt: "17/04/2024",
     id: 4,
   },
   {
-    img: imgPizzaria,
-    title: "PizzaVini",
+    img: ListaTarefas,
+    title: "Lista de Tarefas",
     description:
-      "Um site de uma pizzaria onde o cliente pode fazer seu pedido, consultar valores no carrinho, conhecer mais sobre a pizzaria, como historia, localização e contato, e finalizar seu pedido.",
-    repository: "https://github.com/ViniciusFontes-02/Pizzaria_Vini_deploy",
-    deploy: "https://pizza-vini.vercel.app/",
-    skill: ["React.Js", "Vite.Js", "CSS3"],
+      "Os usuários podem criar, editar e excluir tarefas, enquanto a implementação do TypeScript garante maior confiabilidade e segurança aos dados.",
+    repository: "https://github.com/ViniciusFontes-02/ListaTarefas-REACT-TS",
+    deploy: "https://lista-de-tarefas-react-type-script.vercel.app/",
+    skill: ["React.Js", "TypeScript", "Vite.Js", "CSSModules"],
     createdAt: "17/04/2024",
     id: 5,
   },
   {
-    img: imgPizzaria,
-    title: "PizzaVini",
+    img: GeradordeBoxShadow,
+    title: "Gerador de Box Shadow",
     description:
-      "Um site de uma pizzaria onde o cliente pode fazer seu pedido, consultar valores no carrinho, conhecer mais sobre a pizzaria, como historia, localização e contato, e finalizar seu pedido.",
-    repository: "https://github.com/ViniciusFontes-02/Pizzaria_Vini_deploy",
-    deploy: "https://pizza-vini.vercel.app/",
-    skill: ["React.Js", "Vite.Js", "CSS3"],
+      "Com controles intuitivos, os usuários podem ajustar o deslocamento, o blur, o espalhamento, a cor, a opacidade e a natureza da sombra, seja interna ou externa. Ao finalizar as configurações desejadas, o código gerado é exibido para fácil implementação, permitindo aos usuários copiá-lo com um clique.",
+    repository: "https://github.com/ViniciusFontes-02/gerador-de-Box-Shadow",
+    deploy: "https://viniciusfontes-02.github.io/gerador-de-Box-Shadow/",
+    skill: ["JavaScript", "HTML5", "CSS3"],
     createdAt: "17/04/2024",
     id: 6,
   },
   {
-    img: imgPizzaria,
-    title: "PizzaVini",
+    img: QuizInterativo,
+    title: "Quiz Interativo",
     description:
-      "Um site de uma pizzaria onde o cliente pode fazer seu pedido, consultar valores no carrinho, conhecer mais sobre a pizzaria, como historia, localização e contato, e finalizar seu pedido.",
-    repository: "https://github.com/ViniciusFontes-02/Pizzaria_Vini_deploy",
-    deploy: "https://pizza-vini.vercel.app/",
-    skill: ["React.Js", "Vite.Js", "CSS3"],
+      "Funciona com base nas respostas do usuário, destacando-as em verde se corretas e em vermelho se incorretas, tanto na resposta quanto no fundo da tela. Ao final, exibe o desempenho do usuário e o número de questões corretas. Esse projeto flexível permite a inclusão de várias perguntas, sendo útil para treinar conhecimentos em diversas áreas.",
+    repository: "https://github.com/ViniciusFontes-02/Quiz-Interativo",
+    deploy: "https://viniciusfontes-02.github.io/Quiz-Interativo/",
+    skill: ["JavaScript", "HTML5", "CSS3"],
     createdAt: "17/04/2024",
     id: 7,
   },
   {
-    img: imgPizzaria,
-    title: "PizzaVini",
+    img: GeradordeQRCode,
+    title: "Gerador de QR Code",
     description:
-      "Um site de uma pizzaria onde o cliente pode fazer seu pedido, consultar valores no carrinho, conhecer mais sobre a pizzaria, como historia, localização e contato, e finalizar seu pedido.",
-    repository: "https://github.com/ViniciusFontes-02/Pizzaria_Vini_deploy",
-    deploy: "https://pizza-vini.vercel.app/",
-    skill: ["React.Js", "Vite.Js", "CSS3"],
+      "Permite aos usuários gerar QR Codes facilmente para palavras, frases e URLs. Com um alerta integrado para garantir que o conteúdo seja inserido antes da geração do código, o projeto oferece uma solução simples, porém funcional, que certamente será útil para uma ampla gama de usuários..",
+    repository: "https://github.com/ViniciusFontes-02/Gerador-de-QR-Code",
+    deploy: "https://viniciusfontes-02.github.io/Gerador-de-QR-Code/",
+    skill: ["JavaScript", "HTML5", "CSS3"],
     createdAt: "17/04/2024",
     id: 8,
   },
