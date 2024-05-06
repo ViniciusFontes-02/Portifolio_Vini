c9cda873c79ed9a55d45ad50dd43009dc021e37f
Vinicius Fontes <vinif.santos2002@gmail.com>
2024-05-06 15:57:30 -0300

    Adicionando botão modo dark

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,19 +1,48 @@
-import { faDroplet, faGear } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDroplet,
+  faGear,
+  faMoon,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./DarkMode.module.css";
+import { useEffect, useState } from "react";
 
 export default function DarkMode() {
+  const [theme, setTheme] = useState("light");
+  const [icon, setIcon] = useState(faSun);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("themeSystem");
+    if (savedTheme) {
+      setTheme(savedTheme);
+      defineCurrentTheme(savedTheme);
+      setIcon(savedTheme === "light" ? faSun : faMoon);
+    }
+  }, []);
+
+  function clickDarkBtn() {
+    const newTheme = theme === "light" ? "dark" : "light";
+    localStorage.setItem("themeSystem", newTheme);
+    defineCurrentTheme(newTheme);
+    setTheme(newTheme);
+    setIcon(newTheme === "light" ? faSun : faMoon);
+  }
+
+  function defineCurrentTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+
   return (
     <div className={styles.darkMode}>
       <ul>
-        <li className={styles.iconContainer}>
-          <FontAwesomeIcon icon={faGear} />
-          {/* <span>darkmode</span> */}
+        <li className={styles.iconContainer} onClick={clickDarkBtn}>
+          <FontAwesomeIcon icon={icon} className={styles.icon} />
         </li>
+
         <li className={styles.iconContainer}>
-          <FontAwesomeIcon icon={faDroplet} />
-          {/* <span>cores</span> */}
+          <FontAwesomeIcon icon={faDroplet} className={styles.icon} />
         </li>
       </ul>
     </div>
